979d27aa11925dfa56fa0792e40d0b8270945799
FranciscoSilvaMgLPT <117362612+FranciscoSilvaMgLPT@users.noreply.github.com>
2024-02-19 15:39:00 +0000

    Delete backend-files directory

--- backend-files/src/main/java/com/example/punished/expensetracker/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.punished.expensetracker.repository;
-
-import com.example.punished.expensetracker.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
