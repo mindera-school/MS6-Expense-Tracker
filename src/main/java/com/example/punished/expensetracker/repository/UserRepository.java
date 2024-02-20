7ffd15b15920a3c621f6fb205d8931e93f886b60
PedroBarge <pedro525tavares@gmail.com>
2024-02-20 11:22:43 +0000

    all the Long are now Integer

--- src/main/java/com/example/punished/expensetracker/repository/UserRepository.java
+++ src/main/java/com/example/punished/expensetracker/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.example.punished.expensetracker.repository;
 import com.example.punished.expensetracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
