77886496a873cb996d86b2b95a3d644a3ac8ba16
DaveLRB <148245690+DaveLRB@users.noreply.github.com>
2024-02-20 09:11:58 +0000

    login menu and register menu done, still needs some styling but its ready to use

--- frontend-files/assets/expenseTracker.js
+++ frontend-files/assets/expenseTracker.js
@@ -55,3 +55,21 @@ function sendJSON(nameExpense, amount) {
   const jsonObject = JSON.stringify(object);
   console.log(jsonObject);
 }
+
+let modal = document.getElementById("modal");
+let openModal = document.getElementById("register-menu");
+let closeModal = document.getElementById("close");
+
+openModal.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.onclick = function(event){
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
