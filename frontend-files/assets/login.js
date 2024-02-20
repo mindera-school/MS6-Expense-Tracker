7863699919ed7e6d9d37de0f4c71a4ae2e475575
PedroBarge <pedro525tavares@gmail.com>
2024-02-20 15:46:41 +0000

    login and register still pending

---
+++ frontend-files/assets/login.js
@@ -0,0 +1,48 @@
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let email = document.getElementById("email");
+
+const btnRegister = document.getElementById("register");
+
+function register() {
+  if (username.value === "" || password.value === "" || email.value === "") {
+    alert("Please enter");
+  }
+  sendJSONRegister(username.value, password.value, email.value);
+}
+
+async function sendJSONRegister(username, password, email) {
+  const object = { username, password, email };
+  const jsonObject = JSON.stringify(object);
+  console.log(jsonObject);
+  const response = await fetch("http://localhost:8080/user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonObject,
+  });
+
+  const data = await response.text();
+  console.log(data);
+}
+
+const btnLogin = document.getElementById("login");
+function login(user, password) {
+    sendJSONLogin(user.value, password.value);
+}
+async function sendJSONLogin(user, password) {
+    const object = { user, password };
+    const jsonObject = JSON.stringify(object);
+    console.log(jsonObject);
+    const response = await fetch("http://localhost:8080/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: jsonObject,
+    });
+
+    const data = await response.text();
+    console.log(data);
+}
