212a5f894b26f3591568cc5fd2f5ccae26b6b357
Fábio <fabiogoncalvesk2001@gmail.com>
2024-03-05 17:03:07 +0000

    feat(repository): Add @Repository annotation

--- backend-files/src/main/java/com/example/punished/expensetracker/repository/ExpenseRepository.java
+++ backend-files/src/main/java/com/example/punished/expensetracker/repository/ExpenseRepository.java
@@ -2,6 +2,8 @@ package com.example.punished.expensetracker.repository;
 
 import com.example.punished.expensetracker.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
 }
