d2e8f2098a76297db1a45f047084427c3058f24e
Fábio <fabiogoncalvesk2001@gmail.com>
2024-03-05 17:03:51 +0000

    feat(repository): Add @Repository annotation to UserRepository

--- backend-files/src/main/java/com/example/punished/expensetracker/repository/UserRepository.java
+++ backend-files/src/main/java/com/example/punished/expensetracker/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.punished.expensetracker.repository;
 
 import com.example.punished.expensetracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
